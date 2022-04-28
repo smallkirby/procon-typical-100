c8f4e677ce89a67bf5bf5acac19352e7515a1623
smallkirby <ssmallkirby@gmail.com>
2022-04-29 03:54:26 +0900

    add 7

---
+++ solves/7.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+#define IMAX 1<<29
+#define PI 3.14159265359
+
+using namespace std;
+
+#define rep_(i, a_, b_, a, b, ...) for (int i = (a), i##_len = (b); i < i##_len; ++i)
+#define rep(i, ...) rep_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+
+/******** Utility ***************/
+
+template<typename T>
+void show(vector<T> v)
+{
+  for(auto _v: v)
+    cout << _v << " ";
+  cout << endl;
+}
+
+template<typename T>
+void show(vector<T> v, int maxrow)    // 適度に改行入れるver
+{
+  for(int ix=0; ix!=v.size(); ++ix){
+    if(ix!=0 && ix%maxrow==0)
+      cout << endl;
+    cout << v[ix] << " ";
+  }
+  cout << endl;
+}
+
+template<typename T>
+void show2(vector<vector<T>> v)
+{
+  int ix = 0;
+  for(auto __v: v){
+    cout << "[" << ix++ << "] (";
+    for(auto _v: __v){
+      cout << _v << ", ";
+    }
+    cout << ")" << endl;
+  }
+}
+
+/******** end of Utility ***************/
+
+ulong search(vector<ulong> &A, ulong b) {
+  auto larger = lower_bound(A.begin(), A.end(), b);
+  if (larger == A.end()) return b - A[A.size()-1];
+  if (*larger == b) return 0;
+  auto smaller = larger == A.begin() ? larger : larger - 1;
+
+  return min(*larger - b, b - *smaller);
+}
+
+int main(void)
+{
+  // input
+  ulong N; cin >> N;
+  vector<ulong> A(N);
+  rep(ix, N) cin >> A[ix];
+  ulong Q; cin >> Q;
+  vector<ulong> B(Q);
+  rep(ix, Q) cin >> B[ix];
+
+  sort(A.begin(), A.end());
+
+  // main
+  for (size_t jx = 0; jx != Q; ++jx) {
+    cout << search(A, B[jx]) << endl;
+  }
+
+  // print
+}
