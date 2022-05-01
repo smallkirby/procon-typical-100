3b0640e517dfd82806dd1538522ce0c2c3adb2ac
smallkirby <ssmallkirby@gmail.com>
2022-05-02 02:32:45 +0900

    add abs11

---
+++ solves/abs11.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+
+#define PI 3.14159265359
+
+using namespace std;
+
+#define rep_(i, a_, b_, a, b, ...) for (int i = (a), i##_len = (b); i < i##_len; ++i)
+#define rep(i, ...) rep_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+
+using llong = long long;
+
+/******** Utility ***************/
+
+// Calculate (a ^ b) % mod
+// O(log`mod`)
+llong modpow(llong a, llong b, llong mod) {
+  llong p = 1, q = a;
+  for (int ix = 0; ix != 30; ++ix) {
+    if ((b & (1LL << ix)) != 0) {
+      p *= q;
+      p %= mod;
+    }
+    q *= q;
+    q %= mod;
+  }
+
+  return p;
+}
+
+// Calculate inv of a/b under mod, based on Fermat's little theorem
+// O(log`mod`)
+llong moddiv(llong a, llong b, llong mod) {
+  return (a * modpow(b, mod - 2, mod)) % mod;
+}
+
+vector<llong> _factorial_memo, _factorial_inv_memo;
+void init_factorial_memo(size_t N, llong mod) {
+  if (_factorial_memo.empty()) {
+    _factorial_memo.resize(N+1, 0);
+    _factorial_inv_memo.resize(N+1, 0);
+    _factorial_memo[0] = 1;
+    for (size_t ix = 1; ix <= N; ++ix)
+      _factorial_memo[ix] = (1LL * ix * _factorial_memo[ix - 1]) % mod;
+    for (size_t ix = 0; ix <= N; ++ix)
+      _factorial_inv_memo[ix] = moddiv(1LL, _factorial_memo[ix], mod);
+  }
+}
+
+// O(log`mod`)
+llong nCr(int n, int r, llong mod) {
+  if (n < r || r < 0) return 0;
+  if (_factorial_memo.empty()) init_factorial_memo(n, mod);
+  return (_factorial_memo[n] * _factorial_inv_memo[r] % mod) * _factorial_inv_memo[n - r] % mod;
+}
+
+template<typename T>
+void show(vector<T> v)
+{
+  for(auto _v: v)
+    cout << _v << " ";
+  cout << endl;
+}
+
+template<typename T>
+void show(vector<T> v, int maxrow)    // 適度に改行入れるver
+{
+  for(int ix=0; ix!=v.size(); ++ix){
+    if(ix!=0 && ix%maxrow==0)
+      cout << endl;
+    cout << v[ix] << " ";
+  }
+  cout << endl;
+}
+
+template<typename T>
+void show2(vector<vector<T>> v)
+{
+  int ix = 0;
+  for(auto __v: v){
+    cout << "[" << ix++ << "] (";
+    for(auto _v: __v){
+      cout << _v << ", ";
+    }
+    cout << ")" << endl;
+  }
+}
+
+/******** end of Utility ***************/
+
+int main(void)
+{
+  // input
+  llong N; cin >> N;
+  vector<llong> t(N),x(N),y(N);
+  rep(ix,N) cin >> t[ix] >> x[ix] >> y[ix];
+
+  // main
+  rep(ix, N) {
+    const llong prev_x = ix == 0 ? 0 : x[ix-1];
+    const llong prev_y = ix == 0 ? 0 : y[ix-1];
+    const llong prev_t = ix == 0 ? 0 : t[ix-1];
+    const llong diff = abs(prev_x - x[ix]) + abs(prev_y - y[ix]);
+    if (diff > (t[ix] - prev_t) || ((t[ix] - prev_t) - diff) % 2 == 1) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+
+  // print
+  cout << "Yes" << endl;
+}
